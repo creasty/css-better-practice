ad0d4aaaeda0bfd7e1b89ce32f11945c541a66ea
Yuki Iwanaga <yuki@creasty.com>
2015-07-29 19:40:20 +0900

    First commit

---
+++ compass.rb
@@ -0,0 +1,17 @@
+module SassExtention
+
+  def split(str, delimiter)
+    assert_type(str, :String)
+    assert_type(delimiter, :String)
+
+    parts = str.value.split(delimiter.value)
+      .map { |p| Sass::Script::String.new(p) }
+
+    Sass::Script::List.new(parts, :comma)
+  end
+
+end
+
+module Sass::Script::Functions
+  include SassExtention
+end
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync');
+var gulp         = require('gulp');
+var plumber      = require('gulp-plumber');
+var compass      = require('gulp-compass');
+
+
+gulp.task('server', function() {
+    browserSync({
+        server: {
+            ui:      false,
+            port:    5000,
+            baseDir: './public',
+        },
+        files: [
+            './public/**/*.html',
+            './public/**/*.css',
+        ],
+    });
+});
+
+gulp.task('sass', function () {
+    gulp.src('./scss/{layouts/*,**/shared}/app.scss')
+        .pipe(plumber())
+        .pipe(compass({
+            config_file: './compass.rb',
+            sass:        './scss',
+            css:         './public/css',
+            comments:    true,
+            style:       'expanded',
+            // bundle_exec: true,
+        }))
+        .pipe(autoprefixer({
+            cascade: false,
+        }))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', ['sass', 'server'], function () {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
